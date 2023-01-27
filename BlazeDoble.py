0bf30d5c139387852d7c08888168c35dc0579567
Iago Patrick <100448015+Iago-Patrick@users.noreply.github.com>
2023-01-27 16:31:59 -0300

    mandando sinais atualizados

--- BlazeDoble.py
+++ BlazeDoble.py
@@ -13,65 +13,126 @@ nav.get('https://blaze.com/pt/games/double')
 #pegar informações
 pegar = nav.find_element(By.XPATH, '//*[@id="roulette-recent"]/div').text
 texto=pegar.split()
+print()
+print ('SEM CONVERSAO')
 print (texto)
 
 #converter de string para numero
+org = texto[0:9]
 
-org = texto[0:16]
+#############################################################
 
-pwm = map(number, org)
-
-finalnum = list(pwm)
-
-print(finalnum)
-
-def number(x):
+def number(x): 
+    if x =='0':
+        return 'red'
 
     if x =='1':
-        return 1
+        return 'red'
 
     if x =='2':
-      return 2
+      return 'red'
       
     if x=='3':
-        return 3
+        return 'red'
 
     if x=='4':
-      return 4
+      return 'red'
 
     if x=='5':
-        return 5
+        return 'red'
 
     if x=='6':
-      return 6
+      return 'red'
       
     if x=='7':
-        return 7
+       return 'red'
 
     if x=='8':
-      return 8
+      return 'black'
       
     if x=='9':
-     return 9
+     return 'black'
 
     if x=='10':
-      return 10
+      return 'black'
 
     if x=='11':
-        return 11
+        return 'black'
 
     if x=='12':
-      return 12
+      return 'black'
       
     if x=='13':
-     return 13
+     return 'black'
 
     if x=='14':
-      return 14
+      return 'black'
 
     if x=='15':
-      return 15
+      return 'black'
 
     if x=='16':
-     return 16
+     return 'black'
 
+def color(x):
+  if x < 7:
+    return 'Red'
+  if x > 9:
+   return 'black'
+  if x == 0:
+    return 'white'
+
+def time(x):
+  if x =='5:00':
+   return 1
+  if x =='5:01':
+   return 1
+  if x =='4:90':
+   return 1
+  if x =='4:91':
+   return 1
+  if x =='4:92':
+   return 1
+  if x =='4:93':
+   return 1
+  if x =='4:94':
+   return 1
+  if x =='4:95':
+   return 1
+  if x =='4:99':
+   return 1
+  if x =='4:98':
+   return 1
+  if x =='4:99':
+   return 1
+  if x =='4:97':
+   return 1
+  if x =='4:96':
+   return 1
+  else:
+   return 0
+
+#############################################################
+
+while 1: 
+ pegartempo = nav.find_element(By.XPATH, '//*[@id="roulette-timer"]/div').text
+ texto2=pegartempo.split()
+
+ print()
+ print(texto2)
+ print()
+
+ pwm2 = map(time,texto2)
+ tempofinal = list(pwm2)
+ print(tempofinal)
+ if tempofinal == [0,0,1]:
+   pegar = nav.find_element(By.XPATH, '//*[@id="roulette-recent"]/div').text
+   texto=pegar.split()
+   pwm = map(number, org)
+   finalnum = list(pwm)
+   print()
+   print('COM CONVERSAO')
+   print(finalnum)
+   
+   break
+  
